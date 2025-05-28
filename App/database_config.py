90c5fafc690a883e581945c2a3e8d357bb88b158
levui7 <sofyanewmail@gmail.com>
2025-05-29 00:22:52 +0300

    Add database_config.py

---
+++ App/database_config.py
@@ -0,0 +1,10 @@
+# Конфигурация для подключения к базе данных MySQL
+DB_CONFIG = {
+    'host': 'localhost',
+    'port': '3306',
+    'user': 'root',
+    'password': 'wwssaaddad',
+    'database': 'farm_db'
+}
+
+db_farm_id = 1
